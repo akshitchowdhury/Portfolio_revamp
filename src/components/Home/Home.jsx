0b9cbde3ab1386989d0882686997989f27770fc8
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-02 17:25:20 +0530

    1st svae

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './Home.css'
+
+
+const Home = () => {
+ 
+ 
+ 
+ 
+    return (
+        <>
+
+
+    <div className='backDrop'>
+
+   
+
+
+
+    </div>
+
+    </>
+  )
+}
+
+export default Home
