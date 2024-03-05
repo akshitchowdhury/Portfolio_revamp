a6434c21f6e72a7e6e2a8b234caca832321006f0
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-05 13:41:28 +0530

    about0

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -4,10 +4,12 @@ const AboutUs = () => {
   return (
     <>
     <div className='about'>
-    <div className='flex flex-wrap justify-center'>
-        
-        <h1>About me</h1>
-    </div>
+    <div >
+  <div className='blocks flex flex-row w-full md:w-16 lg:w-96 xl:w-2/5'>
+    <h1 className='text-lg text-white font-bold text-center md:text-center'>About me</h1>
+    <p className='para w-full md:w-1/2  text-xs md:text-lg lg:text-lg xl:text-lg text-white font-bold  '>I'm a passionate MERN developer with an insatiable curiosity for building and experimenting with new technologies. From crafting sleek front-end interfaces to architecting robust back-end systems, I thrive on the full-stack journey. Self-taught and proud of it, I embrace programming challenges with a problem-solving mindset, constantly pushing myself to learn and adapt on the fly. Beyond the realms of web development, I harbor a deep fascination for emerging tech realms such as machine learning, app development, and game development. During my downtime, you'll likely find me immersed in the world of game design, turning ideas into interactive experiences. For me, coding isn't just a profession; it's a journey of perpetual innovation and endless possibilities.</p>
+  </div>
+</div>
     </div>
     </>
   )
