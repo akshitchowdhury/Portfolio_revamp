65920cba4b522e50a1ba053bd7d8871eae49a57b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-10 02:18:32 +0530

    nav7

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -36,6 +36,11 @@ function Navbar() {
     setDropdownPosition({ top: dropdownLinkRect.bottom, left: leftPosition });
   };
 
+  const resetDropdownPosition = () => {
+    setIsDropdownOpen(false);
+    setDropdownPosition({ top: 0, left: 0 });
+  };
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 relative">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -59,13 +64,13 @@ function Navbar() {
         <div className={isDropdownOpen ? "absolute top-full left-0 w-full md:block md:w-auto" : "hidden w-full md:block md:w-auto"} id="navbar-multi-level" style={{ top: dropdownPosition.top, left: dropdownPosition.left }}>
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link to="/" className="navbar-link block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent">Home</Link>
+              <Link to="/" onClick={resetDropdownPosition} className="navbar-link block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent">Home</Link>
             </li>
             <li>
-              <Link to="/about" className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Us</Link>
+              <Link to="/about" onClick={resetDropdownPosition} className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Us</Link>
             </li>
             <li>
-              <Link to="/projects" className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</Link>
+              <Link to="/projects" onClick={resetDropdownPosition} className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</Link>
             </li>
             {!isMobile && (
               <li>
@@ -97,17 +102,14 @@ function Navbar() {
                 </div>
               </li>
             )}
-
             {isMobile &&
             (
               <li>
-              <Link to="/services" className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</Link>
-            </li>
-            )
-            
-            }
+                <Link to="/services" onClick={resetDropdownPosition} className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</Link>
+              </li>
+            )}
             <li>
-              <Link to="/contact" className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+              <Link to="/contact" onClick={resetDropdownPosition} className="navbar-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
             </li>
           </ul>
         </div>
