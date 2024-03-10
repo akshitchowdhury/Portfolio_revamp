c205908ff360fcd54abd92b0a1dd8d5353802a68
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-11 00:48:20 +0530

    gallery1

--- src/components/Projects/Cards/Cards.jsx
+++ src/components/Projects/Cards/Cards.jsx
@@ -15,12 +15,13 @@ const Cards = ({cardData}) => {
           <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{cardData.title}</h5>
         </a>
         <p className="mb-3 text-sm md:text-base font-normal text-gray-700 dark:text-gray-400">{cardData.description}</p>
-        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Read more
-          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-          </svg>
-        </a>
+        <button className="cardButton inline-flex items-center px-3 py-2 text-sm font-medium text-center text-blue-700 bg-transparent border border-blue-700 hover:bg-blue-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:text-blue-300 dark:border-blue-600 dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+  Read more
+  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+  </svg>
+</button>
+
       </div>
     </div>
   );
