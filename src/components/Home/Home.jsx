ae20af0c0309b73365db09503a0ca92e5c191672
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-12 00:00:36 +0530

    home3

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
        <div className= 'profile'>
 
        <p className='text-white max-w-md '>
-       "Welcome to my digital sanctuary! I'm Pratikshit Chowdhury, your guide through this captivating space. If you've found yourself here after clicking on my portfolio link, consider yourself among the cultured few. Explore with curiosity, for within these digital walls lies a tapestry of creativity waiting to be unraveled.
+       Welcome to my digital sanctuary! I'm Pratikshit Chowdhury, your guide through this captivating space. If you've found yourself here after clicking on my portfolio link, consider yourself among the cultured few. Explore with curiosity, for within these digital walls lies a tapestry of creativity waiting to be unraveled.
         </p>
         <img src= {profile} alt="proPic" />
 
