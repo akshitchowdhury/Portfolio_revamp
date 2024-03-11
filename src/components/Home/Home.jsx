6a9b1720e2a6fea2718ab1e23d2af89afff90cbc
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-11 23:07:48 +0530

    home2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,20 +16,21 @@ const Home = () => {
 
      <div className='backDrop'>
 
+      <span className='intro1 text-white'>  {`${"<Welcome>"}`} </span>
        <div className= 'profile'>
 
        <p className='text-white max-w-md '>
-          Hello there...Pratikshit Chowdhury this side..If you clicked on my portfolio link and were redirected to this magnificient space; then it shows that you are a Man/Woman of culture
+       "Welcome to my digital sanctuary! I'm Pratikshit Chowdhury, your guide through this captivating space. If you've found yourself here after clicking on my portfolio link, consider yourself among the cultured few. Explore with curiosity, for within these digital walls lies a tapestry of creativity waiting to be unraveled.
         </p>
         <img src= {profile} alt="proPic" />
 
        
 
        </div>
-
         
+       <span className='intro2 text-white'>  {`${"</Welcome>"}`} </span>
     </div> 
-
+      
     </>
   )
 }
