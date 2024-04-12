28265e899477f6a98b68d59b702a244a201cf989
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-12 18:19:36 +0530

    reChanges4

--- src/components/Services/ServicesCard.jsx
+++ src/components/Services/ServicesCard.jsx
@@ -6,17 +6,12 @@ const ServicesCards = ({cardData}) => {
 
 
   return (
-    <div className="card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img className="rounded-t-lg w-full" src={cardData.image} alt="card1" />
-      </a>
-      <div className="p-5">
-        
-          <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-white dark:text-white" style={{marginLeft: '25%'}}>{cardData.title}</h5>
-        
-
-      </div>
-    </div>
+    <>
+        <img className="card rounded-t-lg w-full  max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" src={cardData.image} alt="card1" />
+      
+  
+    
+    </>
   );
 }
 
